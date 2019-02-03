1a3c0d35bc58672baac215f26e7482a17bba3f25
ShadeP <pettewayshade@gmail.com>
2019-02-02 21:10:59 -0600

    data types

--- test.R
+++ test.R
@@ -1,3 +1,14 @@
+#R programming
 myString <- "Hello World!"
 
 print (myString)
+
+#create a vector
+rainbow <- c('red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet')
+print (rainbow)
+
+#get the class of the vector
+print(class(rainbow))
+
+list1 <- list(c(5,6,7), 10.5, sin)
+print(list1)
